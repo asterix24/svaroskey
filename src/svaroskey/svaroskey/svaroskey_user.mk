24a9324c13c6b3d8e724d4ded8adf54a85dd1320
Daniele Basile <asterix@develer.com>
2016-08-01 10:14:29 +0200

    Add bertos project src.

---
+++ src/svaroskey/svaroskey/svaroskey_user.mk
@@ -0,0 +1,43 @@
+#
+# User makefile.
+# Edit this file to change compiler options and related stuff.
+#
+
+# Programmer interface configuration, see http://dev.bertos.org/wiki/ProgrammerInterface for help
+svaroskey_PROGRAMMER_TYPE = none
+svaroskey_PROGRAMMER_PORT = none
+
+# Files included by the user.
+svaroskey_USER_CSRC = \
+	$(svaroskey_SRC_PATH)/main.c \
+	#
+
+# Files included by the user.
+svaroskey_USER_PCSRC = \
+	#
+
+# Files included by the user.
+svaroskey_USER_CPPASRC = \
+	#
+
+# Files included by the user.
+svaroskey_USER_CXXSRC = \
+	#
+
+# Files included by the user.
+svaroskey_USER_ASRC = \
+	#
+
+# Flags included by the user.
+svaroskey_USER_LDFLAGS = \
+	#
+
+# Flags included by the user.
+svaroskey_USER_CPPAFLAGS = \
+	#
+
+# Flags included by the user.
+svaroskey_USER_CPPFLAGS = \
+	-fno-strict-aliasing \
+	-fwrapv \
+	#
