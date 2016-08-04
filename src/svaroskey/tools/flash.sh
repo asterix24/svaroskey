a91fa759b4129637c9d39a28d52cd0e37c0c6670
Pietro Lorefice <pietro@develer.com>
2016-08-04 15:58:17 +0200

    Add flash script

---
+++ src/svaroskey/tools/flash.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <dev> <bin>"
+    exit 1
+fi
+
+DEV="$1"
+BIN="$2"
+
+if [ ! -e "$DEV" ]; then
+    echo "$DEV: no such file or directory"
+    exit 1
+fi
+
+if [ ! -f "$BIN" ]; then
+    echo "$BIN: no such file or directory"
+    exit 1
+fi
+
+"$DIR/stm32loader" -p "$DEV" -e -w "$BIN"
+
