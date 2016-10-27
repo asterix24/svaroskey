32db3ba52235ed3bd29822e8ad91fa8d37ec18b7
Pietro Lorefice <pietro@develer.com>
2016-10-27 18:16:35 +0200

    [fw] adapt mapping to Petrosky R1

--- src/svaroskey/svaroskey/mappings.c
+++ src/svaroskey/svaroskey/mappings.c
@@ -17,64 +17,39 @@
 KeyMapping keymap_mapping[] = {
 #if (CONFIG_MAPPING_REVISION == 0)
     /* #0 - #11 */
-    { KPA, 0,  KPB, 0  }, { KPA, 0,  KPB, 1  }, { KPA, 0,  KPB, 3  },
-    { KPA, 0,  KPB, 4  }, { KPA, 0,  KPB, 5  }, { KPA, 0,  KPB, 6  },
-    { KPA, 0,  KPB, 7  }, { KPA, 0,  KPB, 8  }, { KPA, 0,  KPB, 9  },
-    { KPA, 0,  KPB, 12 }, { KPA, 0,  KPB, 13 }, { KPA, 0,  KPB, 14 },
+    { KPB, 0,  KPC, 0  }, { KPB, 1,  KPC, 0  }, { KPB, 3,  KPC, 0  },
+    { KPB, 4,  KPC, 0  }, { KPB, 5,  KPC, 0  }, { KPB, 6,  KPC, 0  },
+    { KPB, 7,  KPC, 0  }, { KPB, 8,  KPC, 0  }, { KPB, 9,  KPC, 0  },
+    { KPB, 12, KPC, 0  }, { KPB, 13, KPC, 0  }, { KPB, 14, KPC, 0  },
+
     /* #12 - #23 */
-    { KPA, 1,  KPB, 0  }, { KPA, 1,  KPB, 1  }, { KPA, 1,  KPB, 3  },
-    { KPA, 1,  KPB, 4  }, { KPA, 1,  KPB, 5  }, { KPA, 1,  KPB, 6  },
-    { KPA, 1,  KPB, 7  }, { KPA, 1,  KPB, 8  }, { KPA, 1,  KPB, 9  },
-    { KPA, 1,  KPB, 12 }, { KPA, 1,  KPB, 13 }, { KPA, 1,  KPB, 14 },
+    { KPB, 0,  KPC, 1  }, { KPB, 1,  KPC, 1  }, { KPB, 3,  KPC, 1  },
+    { KPB, 4,  KPC, 1  }, { KPB, 5,  KPC, 1  }, { KPB, 6,  KPC, 1  },
+    { KPB, 7,  KPC, 1  }, { KPB, 8,  KPC, 1  }, { KPB, 9,  KPC, 1  },
+    { KPB, 12, KPC, 1  }, { KPB, 13, KPC, 1  }, { KPB, 14, KPC, 1  },
+
     /* #24 - #35 */
-    { KPA, 4,  KPB, 0  }, { KPA, 4,  KPB, 1  }, { KPA, 4,  KPB, 3  },
-    { KPA, 4,  KPB, 4  }, { KPA, 4,  KPB, 5  }, { KPA, 4,  KPB, 6  },
-    { KPA, 4,  KPB, 7  }, { KPA, 4,  KPB, 8  }, { KPA, 4,  KPB, 9  },
-    { KPA, 4,  KPB, 12 }, { KPA, 4,  KPB, 13 }, { KPA, 4,  KPB, 14 },
+    { KPB, 0,  KPC, 2  }, { KPB, 1,  KPC, 2  }, { KPB, 3,  KPC, 2  },
+    { KPB, 4,  KPC, 2  }, { KPB, 5,  KPC, 2  }, { KPB, 6,  KPC, 2  },
+    { KPB, 7,  KPC, 2  }, { KPB, 8,  KPC, 2  }, { KPB, 9,  KPC, 2  },
+    { KPB, 12, KPC, 2  }, { KPB, 13, KPC, 2  }, { KPB, 14, KPC, 2  },
+
     /* #36 - #47 */
-    { KPA, 5,  KPB, 0  }, { KPA, 5,  KPB, 1  }, { KPA, 5,  KPB, 3  },
-    { KPA, 5,  KPB, 4  }, { KPA, 5,  KPB, 5  }, { KPA, 5,  KPB, 6  },
-    { KPA, 5,  KPB, 7  }, { KPA, 5,  KPB, 8  }, { KPA, 5,  KPB, 9  },
-    { KPA, 5,  KPB, 12 }, { KPA, 5,  KPB, 13 }, { KPA, 5,  KPB, 14 },
-    /* #48 - #60 */
-    { KPA, 6,  KPB, 0  }, { KPA, 6,  KPB, 1  }, { KPA, 6,  KPB, 3  },
-    { KPA, 6,  KPB, 4  }, { KPA, 6,  KPB, 5  }, { KPA, 6,  KPB, 6  },
-    { KPA, 6,  KPB, 7  }, { KPA, 6,  KPB, 8  }, { KPA, 6,  KPB, 9  },
-    { KPA, 6,  KPB, 12 }, { KPA, 6,  KPB, 13 }, { KPA, 6,  KPB, 14 },
-    /* #61 - #72 */
-    { KPA, 7,  KPB, 0  }, { KPA, 7,  KPB, 1  }, { KPA, 7,  KPB, 3  },
-    { KPA, 7,  KPB, 4  }, { KPA, 7,  KPB, 5  }, { KPA, 7,  KPB, 6  },
-    { KPA, 7,  KPB, 7  }, { KPA, 7,  KPB, 8  }, { KPA, 7,  KPB, 9  },
-    { KPA, 7,  KPB, 12 }, { KPA, 7,  KPB, 13 }, { KPA, 7,  KPB, 14 },
-    /* #73 - #84 */
-    { KPA, 8,  KPB, 0  }, { KPA, 8,  KPB, 1  }, { KPA, 8,  KPB, 3  },
-    { KPA, 8,  KPB, 4  }, { KPA, 8,  KPB, 5  }, { KPA, 8,  KPB, 6  },
-    { KPA, 8,  KPB, 7  }, { KPA, 8,  KPB, 8  }, { KPA, 8,  KPB, 9  },
-    { KPA, 8,  KPB, 12 }, { KPA, 8,  KPB, 13 }, { KPA, 8,  KPB, 14 },
-    /* #85 - #96 */
-    { KPA, 9,  KPB, 0  }, { KPA, 9,  KPB, 1  }, { KPA, 9,  KPB, 3  },
-    { KPA, 9,  KPB, 4  }, { KPA, 9,  KPB, 5  }, { KPA, 9,  KPB, 6  },
-    { KPA, 9,  KPB, 7  }, { KPA, 9,  KPB, 8  }, { KPA, 9,  KPB, 9  },
-    { KPA, 9,  KPB, 12 }, { KPA, 9,  KPB, 13 }, { KPA, 9,  KPB, 14 },
-    /* #97 - #108 */
-    { KPA, 10, KPB, 0  }, { KPA, 10, KPB, 1  }, { KPA, 10, KPB, 3  },
-    { KPA, 10, KPB, 4  }, { KPA, 10, KPB, 5  }, { KPA, 10, KPB, 6  },
-    { KPA, 10, KPB, 7  }, { KPA, 10, KPB, 8  }, { KPA, 10, KPB, 9  },
-    { KPA, 10, KPB, 12 }, { KPA, 10, KPB, 13 }, { KPA, 10, KPB, 14 },
-    /* #109 - #120 */
-    { KPA, 13, KPB, 0  }, { KPA, 13, KPB, 1  }, { KPA, 13, KPB, 3  },
-    { KPA, 13, KPB, 4  }, { KPA, 13, KPB, 5  }, { KPA, 13, KPB, 6  },
-    { KPA, 13, KPB, 7  }, { KPA, 13, KPB, 8  }, { KPA, 13, KPB, 9  },
-    { KPA, 13, KPB, 12 }, { KPA, 13, KPB, 13 }, { KPA, 13, KPB, 14 },
-    /* #121 - #132 */
-    { KPA, 14, KPB, 0  }, { KPA, 14, KPB, 1  }, { KPA, 14, KPB, 3  },
-    { KPA, 14, KPB, 4  }, { KPA, 14, KPB, 5  }, { KPA, 14, KPB, 6  },
-    { KPA, 14, KPB, 7  }, { KPA, 14, KPB, 8  }, { KPA, 14, KPB, 9  },
-    { KPA, 14, KPB, 12 }, { KPA, 14, KPB, 13 }, { KPA, 14, KPB, 14 },
-    /* #133 - #144 */
-    { KPA, 15, KPB, 0  }, { KPA, 15, KPB, 1  }, { KPA, 15, KPB, 3  },
-    { KPA, 15, KPB, 4  }, { KPA, 15, KPB, 5  }, { KPA, 15, KPB, 6  },
-    { KPA, 15, KPB, 7  }, { KPA, 15, KPB, 8  }, { KPA, 15, KPB, 9  },
-    { KPA, 15, KPB, 12 }, { KPA, 15, KPB, 13 }, { KPA, 15, KPB, 14 },
+    { KPB, 0,  KPC, 3  }, { KPB, 1,  KPC, 3  }, { KPB, 3,  KPC, 3  },
+    { KPB, 4,  KPC, 3  }, { KPB, 5,  KPC, 3  }, { KPB, 6,  KPC, 3  },
+    { KPB, 7,  KPC, 3  }, { KPB, 8,  KPC, 3  }, { KPB, 9,  KPC, 3  },
+    { KPB, 12, KPC, 3  }, { KPB, 13, KPC, 3  }, { KPB, 14, KPC, 3  },
+
+    /* #48 - #59 */
+    { KPB, 0,  KPC, 4  }, { KPB, 1,  KPC, 4  }, { KPB, 3,  KPC, 4  },
+    { KPB, 4,  KPC, 4  }, { KPB, 5,  KPC, 4  }, { KPB, 6,  KPC, 4  },
+    { KPB, 7,  KPC, 4  }, { KPB, 8,  KPC, 4  }, { KPB, 9,  KPC, 4  },
+    { KPB, 12, KPC, 4  }, { KPB, 13, KPC, 4  }, { KPB, 14, KPC, 4  },
+
+    /* #60 - #71 */
+    { KPB, 0,  KPC, 5  }, { KPB, 1,  KPC, 5  }, { KPB, 3,  KPC, 5  },
+    { KPB, 4,  KPC, 5  }, { KPB, 5,  KPC, 5  }, { KPB, 6,  KPC, 5  },
+    { KPB, 7,  KPC, 5  }, { KPB, 8,  KPC, 5  }, { KPB, 9,  KPC, 5  },
+    { KPB, 12, KPC, 5  }, { KPB, 13, KPC, 5  }, { KPB, 14, KPC, 5  },
 #endif
 };
