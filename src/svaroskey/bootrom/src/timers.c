be6280e8596c6bfe85dbbb4d13487d41399ffbce
Pietro Lorefice <pietro.lorefice@gmail.com>
2016-11-15 08:11:10 +0100

    [boot] SYSTICK peripheral stub

---
+++ src/svaroskey/bootrom/src/timers.c
@@ -0,0 +1,28 @@
+#include "timers.h"
+
+/* SYSTICK CSR register bits */
+#define SYSTICK_CSR_ENABLE	(0x1 << 0)
+#define SYSTICK_CSR_TICKINT	(0x1 << 1)
+#define SYSTICK_CSR_CLKSOURCE	(0x1 << 2)
+#define SYSTICK_CSR_COUNTFLAG	(0x1 << 16)
+
+/* SYSTICK RVR register bits */
+#define SYSTICK_RVR_RELOAD	(0xFFFFFF << 0)
+
+/* SYSTICK CVR register bits */
+#define SYSTICK_CVR_CURRENT	(0xFFFFFF << 0)
+
+void SysTick_Init(void)
+{
+	/* Clear current value */
+	SYSTICK->CVR &= ~SYSTICK_CVR_CURRENT;
+
+	/* Set reload value (@ 72MHz, 1ms) */
+	SYSTICK->RVR = 71999 & SYSTICK_RVR_RELOAD;
+
+	/* Start counting */
+	SYSTICK->CSR |= SYSTICK_CSR_CLKSOURCE |
+			SYSTICK_CSR_TICKINT |
+			SYSTICK_CSR_ENABLE;
+}
+
---
+++ src/svaroskey/bootrom/src/timers.h
@@ -0,0 +1,20 @@
+#ifndef TIMERS_H
+#define TIMERS_H
+
+#include <stdint.h>
+
+/* SYSTICK peripheral definition */
+struct SYSTICK_Periph {
+	volatile uint32_t CSR;
+	volatile uint32_t RVR;
+	volatile uint32_t CVR;
+	volatile uint32_t CALIB;
+};
+
+/* SYSTICK instances */
+#define SYSTICK	((struct SYSTICK_Periph *)(0xE000E010))
+
+/* SYSTICK public methods */
+void SysTick_Init(void);
+
+#endif
