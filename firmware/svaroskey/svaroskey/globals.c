b36767a14779c88ad6f0e72c9b6dd712fbcc852c
Luca Benci <zero@develer.com>
2018-01-21 23:47:32 +0100

    Properly initialize substituted_keys array

--- firmware/svaroskey/svaroskey/globals.c
+++ firmware/svaroskey/svaroskey/globals.c
@@ -4,5 +4,7 @@
 
 key_id_t std_pressed_key_ids[LAYOUT_SIZE] = {0};
 key_id_t custom_pressed_key_ids[MAX_CUSTOM_KEYPRESSES] = {0};
-LogicalKey* substituted_keys[MAX_CUSTOM_KEYPRESSES] = {&noop_lk};
 KeyHash pressed_keys_hash;
+
+// gcc-only initialization
+LogicalKey* substituted_keys[] = {[0 ... MAX_CUSTOM_KEYPRESSES] = &noop_lk};
