bad603075a5b706077d3380f224178fc05268a9d
Daniele Basile <asterix24@gmail.com>
2018-10-22 22:56:05 +0200

    Change structure to manage key beahiviors.

--- firmware/svaroskey/layouts/layouts.h
+++ firmware/svaroskey/layouts/layouts.h
@@ -1,13 +1,44 @@
+/**
+ * \file
+ * <!--
+ * Copyright 2018 Daniele Basile <asterix24@gmail.com>
+ * All Rights Reserved.
+ * -->
+ *
+ * \brief Keyboard Map
+ *
+ * \author Daniele Basile <asterix24@gmail.com>
+ */
 #ifndef SVAROSKEY_LAYOUTS_H
 #define SVAROSKEY_LAYOUTS_H
 
-#include "layout.h"
+#include "keyboard.h"
 
-#include "cfg/cfg_keymap.h"
+#include <cpu/types.h>
 
-extern KeyBinding keymap_layout[LAYOUT_SIZE];
-extern KeyBinding keymap_alternate_layout[LAYOUT_SIZE];
+#include <kern/msg.h>
 
-KeyBinding * layout_get_key(int id);
+#include <drv/usbkbd.h>
+
+#define MAX_PRESSED_KEY  10
+typedef struct {
+	Msg msg;
+	size_t len;
+	uint8_t key_index[MAX_PRESSED_KEY];
+} PressedKeyEvent;
+
+#define NODE_TO_KEYEV(n)   (containerof(containerof((n), Msg, link), PressedKeyEvent, msg))
+#define KEYEV_TO_NODE(ev)  (&((ev)->msg.link))
+
+typedef struct
+{
+	uint16_t scancode;
+	void *extra_level;
+} KeyBinding;
+
+extern KeyBinding keymap_layout[KEYBOARD_LAYOUT_NUM_KEYS];
+
+int layout_usbEvent(UsbKbdEvent *event, PressedKeyEvent *key_ev);
+void layout_init(void);
 
 #endif /* SVAROSKEY_LAYOUTS_H */
