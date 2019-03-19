bc2c0822fe6704ba20c2e64761aebaa91b5923de
Pietro Lorefice <pietro@develer.com>
2019-03-19 12:16:01 +0100

    qmk: match bootloader USB serial number

--- firmware/qmk/keyboards/svaroskey/config.h
+++ firmware/qmk/keyboards/svaroskey/config.h
@@ -24,6 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MANUFACTURER    Develer
 #define PRODUCT         Svaroskey
+#define SERIAL_NUMBER   001
 #define DESCRIPTION     QMK keyboard firmware with ChibiOS
 
 /* key matrix size */
