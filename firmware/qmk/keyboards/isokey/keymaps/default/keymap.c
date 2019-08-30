3d27fa66f30e5df15c93d0ab9cf78846d8d07595
Daniele Basile <asterix24@gmail.com>
2019-08-30 13:56:53 +0200

    qmk: fix layout.

--- firmware/qmk/keyboards/isokey/keymaps/default/keymap.c
+++ firmware/qmk/keyboards/isokey/keymaps/default/keymap.c
@@ -19,6 +19,7 @@
 
 // Define layer names
 #define _NORMAL 0
+#define _EMPTY  1
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -31,12 +32,38 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  | Ctrl    |  Win  |Alt    | space     | space     | AltGr  | Win  |   Menu | Ctrl      | Fn |    | ←        | ↓           | →           |    | 0 Ins               | . Del    |          |
 */
 
+    [_EMPTY] = LAYOUT_empty (
+        KC_A,           KC_A,    KC_A,  KC_A,  KC_A,    KC_A,   KC_A,    KC_A,  KC_A,    KC_A,   KC_A,  KC_A,   KC_A,    KC_A,  KC_A,   KC_A,   KC_A,   KC_A,   KC_A, KC_A, \
+        KC_A, KC_A,    KC_A,    KC_A,  KC_A,  KC_A,    KC_A,   KC_A,    KC_A,  KC_A,    KC_A,   KC_A,  KC_A,   KC_A,    KC_A,  KC_A,   KC_A,   KC_A,   KC_A,   KC_A, KC_A, \
+        KC_A, KC_A,    KC_A,    KC_A,  KC_A,  KC_A,    KC_A,   KC_A,    KC_A,  KC_A,    KC_A,   KC_A,  KC_A,   KC_A,    KC_A,  KC_A,   KC_A,   KC_A,   KC_A,   KC_A, KC_A, \
+        KC_A, KC_A,    KC_A,    KC_A,  KC_A,  KC_A,    KC_A,   KC_A,    KC_A,  KC_A,    KC_A,   KC_A,  KC_A,             KC_A,  KC_A,   KC_A,   KC_A,   KC_A,   KC_A, KC_A, \
+        KC_A, KC_A,    KC_A,    KC_A,  KC_A,  KC_A,    KC_A,   KC_A,    KC_A,  KC_A,    KC_A,   KC_A,  KC_A,                     KC_A,            KC_A,   KC_A,   KC_A,        \
+        KC_A,           KC_A,    KC_A,  KC_A,            KC_A,             KC_A,            KC_A,   KC_A,  KC_A,   KC_A,    KC_A,  KC_A,   KC_A,   KC_A,   KC_A                 \
+    ),
+
     [_NORMAL] = LAYOUT_all (
         KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   \
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_INS,  KC_HOME, KC_PGUP, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_ENT,  KC_DEL,  KC_END,  KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,          KC_NO,   KC_NO,   KC_NO,   KC_P4,   KC_P5,   KC_P6,            \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_RSFT,                   KC_UP,            KC_P1,   KC_P2,   KC_P3,   KC_PENT, \
-        KC_LCTL,   KC_LGUI,    KC_LALT,     KC_SPC,           KC_SPC,           KC_RALT,     KC_RGUI,    KC_APP,   KC_RCTL,  KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT                    \
-    ),
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,          KC_NO,   KC_NO,   KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_PENT, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_RSFT,                   KC_UP,            KC_P1,   KC_P2,   KC_P3,            \
+        KC_LCTL,        KC_LGUI,  KC_LALT,  KC_SPC,           KC_SPC,           KC_RALT,          KC_RGUI, KC_APP, KC_RCTL,  KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT                    \
+    )
 };
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  //debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // If console is enabled, it will print the matrix position and status of each key pressed
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
+#endif
+  return true;
+}
+
