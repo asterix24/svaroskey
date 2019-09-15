89129bf7b5efe65afe53f33b45031a7a057e4f3b
Daniele Basile <asterix24@gmail.com>
2019-09-15 22:57:40 +0200

    isokey: add alternate layout.

--- firmware/qmk/keyboards/isokey/isokey.h
+++ firmware/qmk/keyboards/isokey/isokey.h
@@ -20,7 +20,7 @@
     {  K50,    KC_NO,  K52,    K53,    K44,    KC_NO,  K56,    KC_NO,  K58,    KC_NO,  K510,   K511,   K512,   K513,   K514,   K515,   K516,   KC_NO,  K518,   K519,   KC_NO }  \
 }
 
-#define LAYOUT_empty( \
+#define LAYOUT_alt( \
     K00,            K02,    K03,    K04,    K05,    K06,    K07,    K08,    K09,    K010,   K011,   K012,   K013,   K014,   K015,   K016,   K017,   K018,   K019,   K020,   \
     K10,    K11,    K12,    K13,    K14,    K15,    K16,    K17,    K18,    K19,    K110,   K111,   K112,   K113,   K114,   K115,   K116,   K117,   K118,   K119,   K120,   \
     K20,    K21,    K22,    K23,    K24,    K25,    K26,    K27,    K28,    K29,    K210,   K211,   K212,   K213,   K214,   K215,   K216,   K217,   K218,   K219,   K220,   \
@@ -36,3 +36,4 @@
     {  K40,    K41,    K42,    K43,    K44,    K45,    K46,    K47,    K48,    K49,    K410,   K411,   KC_NO,  K413 ,  KC_NO,  K415,   KC_NO,  K417,   K418,   K419,   KC_NO }, \
     {  K50,    KC_NO,  K52,    K53,    K44,    KC_NO,  K56,    KC_NO,  K58,    KC_NO,  K510,   K511,   K512,   K513,   K514,   K515,   K516,   KC_NO,  K518,   K519,   KC_NO }  \
 }
+
--- firmware/qmk/keyboards/isokey/keymaps/default/keymap.c
+++ firmware/qmk/keyboards/isokey/keymaps/default/keymap.c
@@ -18,10 +18,26 @@
 #include QMK_KEYBOARD_H
 
 // Define layer names
-#define _NORMAL 0
-#define _EMPTY  1
+#define _NORMAL  0
+#define _ALT1    1
+
+enum isokey_keycodes {
+  QMKBEST = SAFE_RANGE,
+  EDA_MS, // Move selection
+  EDA_AG, // Arrange group
+  EDA_ZO, // Zoom out
+  EDA_PM, // Poly Manager
+  EDA_SO, // Selction out
+  EDA_SL, // Selection line
+};
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//   _______,          _______,    _______,  _______,  _______,    _______,   _______,    _______,  _______,    _______,   _______,  _______,   _______,    _______,  _______,   _______,   _______,   _______,   _______, _______,
+//   _______, _______, _______,    _______,  _______,  _______,    _______,   _______,    _______,  _______,    _______,   _______,  _______,   _______,    _______,  _______,   _______,   _______,   _______,   _______, _______,
+//   _______, _______, _______,    _______,  _______,  _______,    _______,   _______,    _______,  _______,    _______,   _______,  _______,   _______,    _______,  _______,   _______,   _______,   _______,   _______, _______,
+//   _______, _______, _______,    _______,  _______,  _______,    _______,   _______,    _______,  _______,    _______,   _______,  _______,               _______,  _______,   _______,   _______,   _______,   _______, _______,
+//   _______, _______, _______,    _______,  _______,  _______,    _______,   _______,    _______,  _______,    _______,   _______,  _______,                         _______,              _______,   _______,   _______,
+//   _______,          _______,    _______,  _______,              _______,               _______,              _______,   _______,  _______,   _______,    _______,  _______,   _______,   _______,   _______
 
 /*  105  LAYOUT
  | Esc   |     | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12       |    | PrtSc    | Scroll Lock | Pause Break |    | X4 Extra | X5 Extra | X6 Extra | X7 Extra |
@@ -31,39 +47,76 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  | Shift | Z   | X   | C   | V   | B   | N   | M   | < , | > . | ? / | XX  | Shift           |               | ↑           |                  | 1 End    | 2 ↓      | 3 PgDn   | Enter    |
  | Ctrl    |  Win  |Alt    | space     | space     | AltGr  | Win  |   Menu | Ctrl      | Fn |    | ←        | ↓           | →           |    | 0 Ins               | . Del    |          |
 */
-
-    [_EMPTY] = LAYOUT_empty (
-        KC_A,           KC_A,    KC_A,  KC_A,  KC_A,    KC_A,   KC_A,    KC_A,  KC_A,    KC_A,   KC_A,  KC_A,   KC_A,    KC_A,  KC_A,   KC_A,   KC_A,   KC_A,   KC_A, KC_A, \
-        KC_A, KC_A,    KC_A,    KC_A,  KC_A,  KC_A,    KC_A,   KC_A,    KC_A,  KC_A,    KC_A,   KC_A,  KC_A,   KC_A,    KC_A,  KC_A,   KC_A,   KC_A,   KC_A,   KC_A, KC_A, \
-        KC_A, KC_A,    KC_A,    KC_A,  KC_A,  KC_A,    KC_A,   KC_A,    KC_A,  KC_A,    KC_A,   KC_A,  KC_A,   KC_A,    KC_A,  KC_A,   KC_A,   KC_A,   KC_A,   KC_A, KC_A, \
-        KC_A, KC_A,    KC_A,    KC_A,  KC_A,  KC_A,    KC_A,   KC_A,    KC_A,  KC_A,    KC_A,   KC_A,  KC_A,             KC_A,  KC_A,   KC_A,   KC_A,   KC_A,   KC_A, KC_A, \
-        KC_A, KC_A,    KC_A,    KC_A,  KC_A,  KC_A,    KC_A,   KC_A,    KC_A,  KC_A,    KC_A,   KC_A,  KC_A,                     KC_A,            KC_A,   KC_A,   KC_A,        \
-        KC_A,           KC_A,    KC_A,  KC_A,            KC_A,             KC_A,            KC_A,   KC_A,  KC_A,   KC_A,    KC_A,  KC_A,   KC_A,   KC_A,   KC_A                 \
-    ),
-
     [_NORMAL] = LAYOUT_all (
-        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   \
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_INS,  KC_HOME, KC_PGUP, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_ENT,  KC_DEL,  KC_END,  KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,          KC_NO,   KC_NO,   KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_PENT, \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_RSFT,                   KC_UP,            KC_P1,   KC_P2,   KC_P3,            \
-        KC_LCTL,        KC_LGUI,  KC_LALT,  KC_SPC,           KC_SPC,           KC_RALT,          KC_RGUI, KC_APP, KC_RCTL,  KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT                    \
-    )
+        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS, KC_AUDIO_VOL_UP,  KC_AUDIO_VOL_DOWN,   KC_AUDIO_MUTE,   QMKBEST,   \
+        KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_COPY, KC_CUT,  KC_PSTE, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
+        KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_ENT,  KC_INS,  KC_HOME, KC_PGUP, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
+        MO(_ALT1),KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,          KC_DEL,  KC_END,  KC_PGDN, KC_P4,   KC_P5,   KC_P6,   KC_PENT, \
+        KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_RSFT,                   KC_UP,            KC_P1,   KC_P2,   KC_P3,            \
+        KC_LCTL,        KC_LGUI,  KC_LALT,  KC_SPC,           KC_SPC,           KC_RALT,          KC_RGUI, KC_APP, KC_RCTL,  MO(_ALT1),   KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT                    \
+    ),
+    [_ALT1] = LAYOUT_alt (
+        _______,          _______,  _______,  _______,  _______,    _______,   _______,    _______,  _______,    _______,   _______,  _______,   _______,    _______,  _______,   _______,   _______,   _______,   _______, _______, \
+        _______, _______, _______,  _______,  _______,  _______,    _______,   _______,    _______,  _______,    _______,   _______,  _______,   _______,    _______,  _______,   _______,   _______,   _______,   _______, _______, \
+        _______, KC_DEL, _______,  _______,  _______,  _______,    _______,   _______,    _______,  _______,    _______,   _______,  _______,   _______,    _______,  _______,   _______,   _______,   _______,   _______, _______, \
+        _______, EDA_MS,  EDA_AG,   EDA_ZO,  EDA_PM,  _______,    KC_LEFT,   KC_DOWN,    KC_UP,    KC_RGHT,    _______,   _______,  _______,               _______,  _______,   _______,   _______,   _______,   _______, _______, \
+        _______, EDA_SO,  EDA_SL,  _______,  _______,  _______,    _______,   _______,    _______,  _______,    _______,   _______,  _______,                         _______,              _______,   _______,   _______,          \
+        _______,          _______,  _______,  _______,              _______,               _______,              _______,   _______,  _______,   _______,    _______,  _______,   _______,   _______,   _______                      \
+    ),
 };
 
 void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  //debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
+    // Customise these values to desired behaviour
+    debug_enable=true;
+    //debug_matrix=true;
+    //debug_keyboard=true;
+    ////debug_mouse=true;
 }
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // If console is enabled, it will print the matrix position and status of each key pressed
 #ifdef CONSOLE_ENABLE
     uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
 #endif
-  return true;
+    switch (keycode) {
+        case EDA_MS: // Move selection
+            if (record->event.pressed) {
+                // when keycode QMKBEST is pressed
+                SEND_STRING("ms");
+            }
+        break;
+        case EDA_AG: // Arrange group
+            if (record->event.pressed) {
+                // when keycode QMKBEST is pressed
+                SEND_STRING("ag");
+            }
+        break;
+        case EDA_ZO: // Zoom out
+            if (record->event.pressed) {
+                // when keycode QMKBEST is pressed
+                SEND_STRING("zo");
+            }
+        break;
+        case EDA_PM: // Poly Manager
+            if (record->event.pressed) {
+                // when keycode QMKBEST is pressed
+                SEND_STRING("pm");
+            }
+        break;
+        case EDA_SO: // Selction out
+            if (record->event.pressed) {
+                // when keycode QMKBEST is pressed
+                SEND_STRING("so");
+            }
+        break;
+        case EDA_SL: // Selection line
+            if (record->event.pressed) {
+                // when keycode QMKBEST is pressed
+                SEND_STRING("sl");
+            }
+        break;
+    }
+    return true;
 }
 
